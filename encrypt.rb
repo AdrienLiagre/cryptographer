f5ecf98a6037d1bdbf73733c88eaf70209c89308
Vincent Cornière <vincent.corniere@gmail.com>
2017-06-14 20:44:40 +0200

    simplyfy and add input from terminal

--- encrypt.rb
+++ encrypt.rb
@@ -1,46 +1,35 @@
 
 class EncryptionEngine
-  def encrypt(message)
-    array_letters = message.split("")
+  KEY = 13
 
-    array_letters.map! do |letter|
-      encrypt_letter(letter)
-    end
-    array_letters.join("")
+  def encrypt(message)
+    message.split("").map! { |letter| encrypt_letter(letter) }.join('')
   end
 
-
-
   def decrypt(crypt_message)
-    array_letters = crypt_message.split("")
-    array_letters.map! do |letter|
-      encrypt_letter(letter)
-    end
-    array_letters.join("")
+    crypt_message.split("").map! { |letter| encrypt_letter(letter) }.join('')
   end
 
   private
 
   def encrypt_letter(letter)
     return ' ' if letter == ' '
-    alphabet = if letter_uppercase?(letter)
-      ('A'..'Z').to_a
-    else
-      ('a'..'z').to_a
-    end
+    alphabet = letter_uppercase?(letter) ? ('A'..'Z').to_a : ('a'..'z').to_a
 
-    letter_index = alphabet.index(letter)
-    letter_13 = letter_index + 13
-    letter_13 -= 26 if letter_13 >= 26
-    alphabet[letter_13]
+    letter_index = alphabet.index(letter) + KEY
+    letter_index -= 26 if letter_index >= 26
+    alphabet[letter_index]
   end
 
   def letter_uppercase?(letter)
     letter == letter.upcase
   end
 end
-  engine = EncryptionEngine.new
-  message_encrypt = engine.encrypt("Mon MeSSage")
-  puts message_encrypt
-  puts engine.decrypt(message_encrypt)
+
+puts "Votre message"
+message = gets.chomp
+engine = EncryptionEngine.new
+message_encrypt = engine.encrypt(message)
+puts message_encrypt
+puts engine.decrypt(message_encrypt)
 
