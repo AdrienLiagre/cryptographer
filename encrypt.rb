aa7509cee2c882e6bf1cc1a6c15bca217015d0c2
Vincent Cornière <vincent.corniere@gmail.com>
2017-06-14 20:26:32 +0200

    add uppercase and empty letter

--- encrypt.rb
+++ encrypt.rb
@@ -1,18 +1,6 @@
 
 class EncryptionEngine
-
-  def encrypt_letter(letter)
-
-    alphabet = ('A'..'Z').to_a
-    letter_index = alphabet.index(letter)
-
-    letter_13 = letter_index + 13
-    letter_13 -= 26 if letter_13 >= 26
-    alphabet[letter_13]
-  end
-
   def encrypt(message)
-
     array_letters = message.split("")
 
     array_letters.map! do |letter|
@@ -26,7 +14,27 @@ class EncryptionEngine
   def decrypt(string)
 
   end
+
+  private
+
+  def encrypt_letter(letter)
+    return ' ' if letter == ' '
+    alphabet = if letter_uppercase?(letter)
+      ('A'..'Z').to_a
+    else
+      ('a'..'z').to_a
+    end
+
+    letter_index = alphabet.index(letter)
+    letter_13 = letter_index + 13
+    letter_13 -= 26 if letter_13 >= 26
+    alphabet[letter_13]
+  end
+
+  def letter_uppercase?(letter)
+    letter == letter.upcase
+  end
 end
   engine = EncryptionEngine.new
-  puts engine.encrypt("ADRIEN")
+  puts engine.encrypt("Mon MeSSage")
 
